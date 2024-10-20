e300fedf5d00de2a54a0045393370868f2ba4cb9
EWART-boop <johnewartdelacruz@gmail.com>
2024-10-20 15:33:52 +0800

    arrayActivity.js.

--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const favSingers = ["Billie Eilish", "Sam Smith", "Adele"];
-console.log(favSingers[0]);
-const favNumbers = ["10", "27", "19", "20"];
-const mixedArr = ["string", ["otherarray"], 123, true];
-
-console.log(mixedArr[0]); 
-console.log(mixedArr[1]); 
-console.log(mixedArr[2]); 
-console.log(mixedArr[3]);
