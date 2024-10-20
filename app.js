e3eb50ae8e5a64d32063d83c4664ab0930a118ca
EWART-boop <johnewartdelacruz@gmail.com>
2024-10-20 15:32:21 +0800

    arrayActivity.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const favSingers = ["Billie Eilish", "Sam Smith", "Adele"];
+
+const favNumbers = ["10", "27", "19", "20"];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers[0]);
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]);
